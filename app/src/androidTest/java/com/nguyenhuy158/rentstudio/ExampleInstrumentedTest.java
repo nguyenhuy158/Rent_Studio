14bc8173401b36622948b2e6e5707d1d27477e56
Nguyen Huy <hyquaq@gmail.com>
2022-12-21 01:12:39 +0700

    update .app

--- app/src/androidTest/java/com/nguyenhuy158/rentstudio/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nguyenhuy158/rentstudio/ExampleInstrumentedTest.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 12/18/22, 5:18 PM Nguyen Huy
+ *
+ * ExampleInstrumentedTest.java [lastModified: 12/15/22, 2:24 PM]
+ *
+ * Contact:
+ * facebook: https://www.facebook.com/nguyenhuy158/
+ * github: https://www.github.com/nguyenhuy158/
+ */
+
 package com.nguyenhuy158.rentstudio;
 
 import android.content.Context;
--- app/src/test/java/com/nguyenhuy158/rentstudio/ExampleUnitTest.java
+++ app/src/test/java/com/nguyenhuy158/rentstudio/ExampleUnitTest.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 12/18/22, 5:18 PM Nguyen Huy
+ *
+ * ExampleUnitTest.java [lastModified: 12/15/22, 2:24 PM]
+ *
+ * Contact:
+ * facebook: https://www.facebook.com/nguyenhuy158/
+ * github: https://www.github.com/nguyenhuy158/
+ */
+
 package com.nguyenhuy158.rentstudio;
 
 import org.junit.Test;
